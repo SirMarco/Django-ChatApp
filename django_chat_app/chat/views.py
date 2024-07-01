ed7de3b6750199609b92b5daf57af80fa764da52
Marco Engelhardt <sirmarco@reussnet.ch>
2024-07-01 15:09:10 +0200

    show chat messages

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 def index(request):
-  return render(request, 'chat/index.html', {'username': 'Marco'})
+  if request.method == 'POST':
+    print("Revived Data: " + request.POST['textmessage'])
+    testChat = Chat.objects.get(id=1) 
+    # create ein neues object vom Typ Message. 
+    Message.objects.create(text=request.POST['textmessage'], chat=testChat, author=request.user, receiver=request.user)
+  chatMessages = Message.objects.filter(chat__id=1)    
+  return render(request, 'chat/index.html', {'messages': chatMessages})
