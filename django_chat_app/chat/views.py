cf3d779116459e7241b6455374a8213b9cc06004
Marco Engelhardt <sirmarco@reussnet.ch>
2024-07-07 11:16:05 +0200

    login

--- django_chat_app/chat/views.py
+++ django_chat_app/chat/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Message, Chat
+from django.contrib.auth import authenticate, login
 
 def index(request):
   if request.method == 'POST':
@@ -9,3 +11,15 @@ def index(request):
     Message.objects.create(text=request.POST['textmessage'], chat=testChat, author=request.user, receiver=request.user)
   chatMessages = Message.objects.filter(chat__id=1)    
   return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+       user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+       if user:
+          login(request, user)
+          return HttpResponseRedirect('/chat/')
+       else:
+          print('user wrong')
+          return render(request, 'auth/login.html', {'wrongPasswort': True})
+       
+    return render(request, 'auth/login.html')
